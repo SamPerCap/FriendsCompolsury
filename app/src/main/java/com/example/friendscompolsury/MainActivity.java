0447e698de7f676d303789cf0741874af0bd7a3b
SamPerCap <31726611+SamPerCap@users.noreply.github.com>
2019-03-25 15:46:42 +0100

    Small button to get the name of the person with ID 0

--- app/src/main/java/com/example/friendscompolsury/MainActivity.java
+++ app/src/main/java/com/example/friendscompolsury/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.friendscompolsury;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String TAG = "Developer";
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = this.findViewById(R.id.btnGetPerson);
+    }
+    void getPersons(){
+        SQLiteImplementation sql = new SQLiteImplementation(this);
+
+        button.setText(sql.getPersonById(0).m_name);
     }
 }
