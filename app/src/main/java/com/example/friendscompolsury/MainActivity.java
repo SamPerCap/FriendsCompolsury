07a12b4a1434db80c2bf96dd1149dd2e4fcc74a6
liverpoljer <liverpoljer@live.dk>
2019-04-09 12:49:17 +0200

    premision

--- app/src/main/java/com/example/friendscompolsury/MainActivity.java
+++ app/src/main/java/com/example/friendscompolsury/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         _dataAccess = new DataAccessFactory();
         _dataAccess.init(MainActivity.this);
         context = this;
-        readPremision();
+        askPremision();
 
         list = (ListView) findViewById(R.id.listview);
         SettingAdapter();
@@ -65,20 +65,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void readPremision()
+    private void askPremision()
     {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
 
 
-            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_DENIED) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_DENIED
+                        ||checkSelfPermission(Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_DENIED
+                        ||checkSelfPermission(Manifest.permission.SEND_SMS)
+                        == PackageManager.PERMISSION_DENIED
+                        ||checkSelfPermission(Manifest.permission.CALL_PHONE)
+                        == PackageManager.PERMISSION_DENIED
+                        ||checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_DENIED
+                        ||checkSelfPermission(Manifest.permission.INTERNET)
+                        == PackageManager.PERMISSION_DENIED
+                        ||checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_DENIED
+                        ||checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_DENIED
+                        ||checkSelfPermission(Manifest.permission.ACCESS_NETWORK_STATE)
+                        == PackageManager.PERMISSION_DENIED) {
 
-                Log.d(TAG, "permission denied to CAMERA - requesting it");
-                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                    Log.d(TAG, "permission denied to CAMERA - requesting it");
+                    String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE
+                            ,Manifest.permission.CAMERA
+                            ,Manifest.permission.SEND_SMS
+                            ,Manifest.permission.CALL_PHONE
+                            ,Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            ,Manifest.permission.INTERNET
+                            ,Manifest.permission.ACCESS_FINE_LOCATION
+                            ,Manifest.permission.ACCESS_COARSE_LOCATION
+                            ,Manifest.permission.ACCESS_NETWORK_STATE};
 
-                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
-            }
+                    requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+                }
         }
+
     }
 
     private void SettingAdapter() {
