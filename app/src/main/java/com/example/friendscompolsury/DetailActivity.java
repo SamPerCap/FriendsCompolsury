965d07e812f9f9c428483cc99dffdf52a0e97a77
SamPerCap <31726611+SamPerCap@users.noreply.github.com>
2019-04-09 14:14:59 +0200

    missing an import

--- app/src/main/java/com/example/friendscompolsury/DetailActivity.java
+++ app/src/main/java/com/example/friendscompolsury/DetailActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
